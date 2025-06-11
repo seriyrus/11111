aa4890a05e8f7d1768aee600b14dc377fa6c6292
seriyrus <postolenko00@bk.ru>
2025-06-11 11:32:18 +0300

    1

--- Victorina/main/models.py
+++ Victorina/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length= 500)
+    v1 = models.CharField(max_length= 500)
+    v2 = models.CharField(max_length= 500)
+    v3= models.CharField(max_length= 500)
+    v4 = models.CharField(max_length= 500)
+    
+    
+    CR = [
+        ('1', v1),
+        ('2', v2),
+        ('3', v3),
+        ('4', v4),
+    ]
+    
+    correct = models.CharField(max_length=500, choises = CR)
